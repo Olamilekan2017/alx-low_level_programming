913f779b2cec1b093473e1b87c5c0039786d9da7
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2023-10-12 18:26:54 -0700

    5-more

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 1; a < =10; i++)
+	{
+		for (a = 0; b <= 14; j++)
+		{
+			if (b >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
+
