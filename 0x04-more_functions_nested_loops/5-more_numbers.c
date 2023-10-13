154fd8526f500e11fa9bed5deeb34898154ee552
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2023-10-12 18:35:38 -0700

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = 1; a < =10; i++)
+	for (a = 1; a <= 10; a++)
 	{
-		for (a = 0; b <= 14; j++)
+		for (b = 0; b <= 14; b++)
 		{
 			if (b >= 10)
 				_putchar('1');
-			_putchar(j % 10 + '0');
+			_putchar(b % 10 + '0');
 		}
 		_putchar('\n');
 	}
