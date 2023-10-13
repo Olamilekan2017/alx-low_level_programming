7c4c9d8a68c46b1076bb7c8b2b6806334c3bb56e
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2023-10-12 22:21:09 -0700

    100-print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: the integer
+ * Result: void
+ */
+
+void print_number(int n)
+{
+	unsigned int m1;
+
+	if (n < 0)
+	{
+		m1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		m1 = n;
+	}
+
+	if (m1 / 10)
+	{
+		print_number(m1 / 10);
+	}
+	_putchar((m1 % 10) + '0');
+}
