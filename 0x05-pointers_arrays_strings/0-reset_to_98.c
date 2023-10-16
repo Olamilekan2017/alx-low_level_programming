422f7c419a493757d56960c323cb6d97d34f0738
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2023-10-16 09:36:50 -0700

    0-98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - This function takes a pointer to an int
+ * @n: The parameter of the function
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+arrays_strings_pointers readme
