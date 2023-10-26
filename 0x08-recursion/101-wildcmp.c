6c1e93bd1b2f9efb0c74cd57ab4e00b776316bc9
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2023-10-25 22:21:58 -0700

    101-wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * wildcmp - This function compares two strings
+ * @s1: The first parameter of the function
+ * @s2: The second parameter of the function
+ * Return: 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
