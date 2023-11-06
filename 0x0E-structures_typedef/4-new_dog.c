a30c34ff6995fc0b91b6f72078ff72d6fcae1ec2
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2023-11-06 10:50:37 -0800

    4-new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,40 +12,40 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	unsigned int a, b, c;
-	dog_t *dog;
+	dog_t *p;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
+	p = malloc(sizeof(dog_t));
+	if (p == NULL)
 	{
-		free(dog);
+		free(p);
 		return (NULL);
 	}
-	for (a = 0; name[0] != '\0'; a++)
+	for (a = 0; name[a] != '\0'; a++)
 		;
 	a++;
-	dog->name = malloc(sizeof(char) * a);
-	if (dog->name == NULL)
+	p->name = malloc(sizeof(char) * a);
+	if (p->name == NULL)
 	{
-		free(dog->name);
-		free(dog);
+		free(p->name);
+		free(p);
 		return (NULL);
 	}
 	for (c = 0; c < a; c++)
-		dog->name[c] = name[c];
-	dog->age = age;
-	for (b = 0; owner[0] != '\0'; b++)
+		p->name[c] = name[c];
+	p->age = age;
+	for (b = 0; owner[b] != '\0'; b++)
 		;
 	b++;
-	dog->owner = malloc(sizeof(char) * b);
-	if (dog->owner == NULL)
+	p->owner = malloc(sizeof(char) * b);
+	if (p->owner == NULL)
 	{
-		free(dog->owner);
-		free(dog);
+		free(p->owner);
+		free(p);
 		return (NULL);
 	}
 	for (c = 0; c < b; c++)
-		dog->owner[c] = owner[c];
-	return (dog);
+		p->owner[c] = owner[c];
+	return (p);
 }
