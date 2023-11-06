c030ce3cdfecc729514cf03ba6da5c7372b34334
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2023-11-06 07:06:35 -0800

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - This define a new type struct dog
+ * @name: The first parameter of the function
+ * @owner: The second parameter of the function
+ * @age: The third parameter of the function
+ *
+ * Description: This is my first struct project with alx
+ */
+
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
+
 #endif
