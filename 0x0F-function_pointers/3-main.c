2f943efb326c47015c098244922c2dbdca4ada51
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2023-11-08 16:59:09 -0800

    main,function,get_op_func,calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(1);
+		exit(98);
 	}
 	num1 = atoi(argv[1]);
 	op = (argv[2]);
@@ -25,12 +25,12 @@ int main(int argc, char *argv[])
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
-		exit(2);
+		exit(99);
 	}
 	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
-		exit(3);
+		exit(100);
 	}
 	printf("%d\n", get_op_func(op) (num1, num2));
 	return (0);
