d4af1604abffb4efe2b1094152cd2580abd17ae0
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2023-11-16 12:12:44 -0800

    1-list_len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,18 +3,20 @@
 /**
  * list_len - This function returns the number of elements
  * @h: The only parameter of the function
- * Return: Total
+ * Return: total
  */
 
 size_t list_len(const list_t *h)
 {
-	unsigned int total = 0;
+	int total = 1;
 
 	if (h == NULL)
 		return (0);
 
 	while (h->next != NULL)
+	{
 		total++;
-	h = h->next;
+		h = h->next;
+	}
 	return (total);
 }
