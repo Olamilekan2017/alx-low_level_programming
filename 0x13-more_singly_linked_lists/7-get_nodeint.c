8889245c5577eace394c79de089d6e061c7baeb9
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2023-11-21 16:28:00 -0800

    7-get_nodeint

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,11 +11,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	if (head == NULL)
-		return (NULL);
 	while (i < index)
 	{
 		i++;
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
 	}
 	return (head);
