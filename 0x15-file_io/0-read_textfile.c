10a2118ad0d551887ae2f99e391e3a3f7e6b23b2
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2023-11-27 16:06:53 -0800

    0-read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t r, w;
 	char *buff;
 
-	if (filename == 0)
+	if (!filename)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
