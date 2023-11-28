698f45f94df69339647345da0cb813122acd6945
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2023-11-27 16:50:51 -0800

    0-read_textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,29 +2,32 @@
 
 /**
  * read_textfile - This function reads a text file
+ * and print to POSIX STDOUT
  * @filename: The first parameter
  * @letters: The second parameter
- * Return: number
+ * Return: number OF letters printed
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t nr, nwr;
+	ssize_t nrd, nwr;
 	char *buff;
 
 	if (filename == 0)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
+
 	if (fd == -1)
 		return (0);
 
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 		return (0);
-	nr = read(fd, buff, letters);
-	nwr = write(STDOUT_FILENO, buff, nr);
+
+	nrd = read(fd, buff, letters);
+	nwr = write(STDOUT_FILENO, buff, nrd);
 
 	close(fd);
 	free(buff);
