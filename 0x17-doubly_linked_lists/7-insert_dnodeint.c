17203d30bce4ffeb660cdf90c18bb9f191767134
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-01-11 15:57:28 -0800

    insert_dnodeint

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,22 +1,4 @@
 #include "lists.h"
-listint_t *create_new_node(int n);
-/**
- * create_new_node - This is the new node function
- * @n: The only parameter
- * Return: pointer
- */
-listint_t *create_new_node(int n)
-{
-	listint_t *new_node;
-
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = NULL;
-	return (new_node);
-}
-
 /**
  * insert_dnodeint_at_index - This function inserts a new node
  * @h: The head pointer
@@ -27,38 +9,38 @@ listint_t *create_new_node(int n)
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *new_node, *temp = *h;
 	unsigned int i = 0;
-	listint_t *tmp;
-	listint_t *tmp_o;
-	listint_t *new_node;
 
-	tmp = *head;
-	if (head == NULL)
+	if (h == NULL)
 		return (NULL);
-	new_node = create_new_node(n);
+	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-	if (*head == NULL)
+	new_node->n = n;
+	new_node->next = NULL;
+	if (idx == 0)
 	{
-		*head = new_node;
+		new_node->next = *h;
+		if (*h != NULL)
+			(*h)->prev = new_node;
+		*h = new_node;
 		return (new_node);
 	}
-	if (idx == 0)
-		*head = new_node;
-	while (i < idx - 1 && tmp != NULL && idx != 0)
+	while (temp != NULL && i < idx - 1)
 	{
+		temp = temp->next;
 		i++;
-		tmp = tmp->next;
 	}
-	if (tmp == NULL)
-		return (NULL);
-	if (idx == 0)
-		new_node->next = tmp;
-	else
+	if (temp == NULL)
 	{
-		tmp_o = tmp->next;
-		tmp->next = new_node;
-		new_node->next = tmp_o;
+		free(new_node);
+		return (NULL);
 	}
+	new_node->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
+	temp->next = new_node;
+	new_node->prev = temp;
 	return (new_node);
-}	
+}
