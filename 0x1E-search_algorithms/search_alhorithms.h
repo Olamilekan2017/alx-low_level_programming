4a7a988b3b1085e817aab8baead06e2d26bd06d6
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-05-09 16:53:22 -0700

    header

---
+++ 0x1E-search_algorithms/search_alhorithms.h
@@ -0,0 +1,15 @@
+#ifndef SEARCH_ALGORITHMS_HEADER_
+#define SEARCH_ALGORITHMS_HEADER_
+
+#include <<stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+
+#endif
